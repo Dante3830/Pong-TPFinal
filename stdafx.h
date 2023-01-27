ee64533b9c1a2a99201015c76b03621e6d9aa180
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-01-27 16:55:43 -0300

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+
+#ifdef SFML_SYSTEM_IOS
+#include <SFML/Main.hpp>
+#endif
+
+using namespace sf;
