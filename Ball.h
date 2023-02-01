416516efa5654a0cd8b70532b3b9af05a5710fe3
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-02-01 17:40:55 -0300

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -14,87 +14,93 @@ public:
     RectangleShape getBallObject() {
         return BallObject;
     }
-    Ball(double x, double y);
-    FloatRect getBallFloatRect();
-    void reboundSides();
-    void passLeft();
+
+    Ball(double x, double y) {
+        ballPosition.x = x;
+        ballPosition.y = y;
+        BallObject.setSize(sf::Vector2f(10, 10));
+
+        BallObject.setPosition(ballPosition);
+
+        BallObject.setFillColor(sf::Color::White);
+
+        //Tengo que poder hacer que me cargue el audio
+        //if (!buffer.loadFromFile("C:\\Users\\Leo\\Desktop\\Pong\\Audios\\PongHit2.wav")) {
+        //     cout << "Error. No se pudo cargar audio." << fs::current_path() << endl;
+        // }
+
+        //sound.setBuffer(buffer);
+    }
+
+    FloatRect getBallFloatRect() {
+        return BallObject.getGlobalBounds();
+    }
+    
+    void reboundSides() {
+        
+        if (ballPosition.y > windowHeight) {
+            ballVelocityY *= -1;
+        }
+        else if (ballPosition.y < 0) {
+            ballVelocityY *= -1;
+        }
+
+        sound.play();
+
+    }
+
+    void passLeft() {
+        if (ballPosition.x < 0) {
+            batscore++;
+            ballPosition.x = windowWidth / 2;
+            ballPosition.y = windowHeight / 2;
+
+            if (rand() % 2 == 1) {
+                ballVelocityY *= -1;
+            } else if (rand() % 2 == 2) {
+                ballVelocityX *= -1;
+            }
+        }
+    }
+
     Vector2f getBallPosition;
-    void passRight();
-    void intersectBat();
-    void intersectAIBat();
-    void reboundBatorAI();
-    void update();
+    
+    void passRight() {
+        if (ballPosition.x > windowWidth) {
+            ballPosition.x = windowWidth / 2;
+            ballPosition.y = windowHeight / 2;
+
+            if (rand() % 2 == 1) { 
+                ballVelocityY *= -1; 
+            } else if (rand() % 2 == 2) { 
+                ballVelocityX *= -1; 
+            }
+        }
+    }
+
+    void reboundBatorAI() {
+        ballPosition.x -= (ballVelocityX * 30);
+        ballVelocityX *= -1;
+    }
+
+    void update() {
+        ballPosition.x += ballVelocityX;
+        ballPosition.y += ballVelocityY;
+
+        BallObject.setPosition(ballPosition);
+    }
+
     double ballVelocityX = 0.5f;
     double ballVelocityY = 0.5f;
-    void stop();
-    void go();
+    
+    void stop() {
+        ballVelocityY = 0;
+        ballVelocityX = 0;
+    }
+
+    void go() {
+        ballVelocityY = 0.5f;
+        ballVelocityX = 0.5f;
+    }
 
 };
-
-// BALL functions
-
-Ball::Ball(double x, double y) {
-    ballPosition.x = x;
-    ballPosition.y = y;
-    BallObject.setSize(sf::Vector2f(10, 10));
-
-    BallObject.setPosition(ballPosition);
-
-    BallObject.setFillColor(sf::Color::White);
-}
-
-void Ball::update() {
-    ballPosition.x += ballVelocityX;
-    ballPosition.y += ballVelocityY;
-
-    BallObject.setPosition(ballPosition);
-}
-
-void Ball::reboundSides() {
-    if (ballPosition.y > windowHeight) {
-        ballVelocityY *= -1;
-    }
-    else if (ballPosition.y < 0) {
-        ballVelocityY *= -1;
-    }
-}
-
-void Ball::passLeft() {
-    if (ballPosition.x < 0) {
-        batscore++;
-        ballPosition.x = windowWidth / 2;
-        ballPosition.y = windowHeight / 2;
-
-        if (rand() % 2 == 1) ballVelocityY *= -1;
-        if (rand() % 2 == 2) ballVelocityX *= -1;
-    }
-}
-
-void Ball::passRight() {
-    if (ballPosition.x > windowWidth ) {
-        ballPosition.x = windowWidth / 2;
-        ballPosition.y = windowHeight / 2;
-        lives--;
-        if (rand() % 2 == 1) ballVelocityY *= -1;
-        if (rand() % 2 == 2) ballVelocityX *= -1;
-    }
-}
-
-FloatRect Ball::getBallFloatRect() {
-    return BallObject.getGlobalBounds();
-}
-
-void Ball::reboundBatorAI() {
-    ballPosition.x -= (ballVelocityX * 30);
-    ballVelocityX *= -1;
-}
-
-void Ball::stop() {
-    ballVelocityY = 0;
-    ballVelocityX = 0;
-}
-
-void Ball::go() {
-    ballVelocityY = 0.5f;
-    ballVelocityX = 0.5f;
-}
