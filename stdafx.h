d79445319025d9afdf36c95dec9e9d9126ad71cf
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-02-01 17:43:26 -0300

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -6,9 +6,14 @@
 #include <ctime>
 #include <cstdlib>
 #include <sstream>
+#include <iostream>
+#include <filesystem>
 
 #ifdef SFML_SYSTEM_IOS
 #include <SFML/Main.hpp>
 #endif
 
 using namespace sf;
+using namespace std;
+
+namespace fs = std::filesystem;
