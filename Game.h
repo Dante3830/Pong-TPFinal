bea4c666ea222a360442102abbbad3595daf1094
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-02-14 12:21:46 -0300

    Update Game.h

--- Game.h
+++ Game.h
@@ -7,11 +7,91 @@
 #include "Constants.h"
 
 class Game {
-	public:
+private:
+    Font font;
+    SoundBuffer buffer;
+    Sound music;
 
-    Game(){}
+    Text title;
+    Text startMessage;
 
-	void pong() {
+    Text _BatScore;
+    Text _aiBatScore;
+
+    Text finishMessage1;
+    Text finishMessage2;
+
+    void updateBatScore() {
+        char bts[10];
+        _itoa_s(batScore, bts, 10);
+        _BatScore.setString(bts);
+    }
+
+    void updateAiBatScore() {
+        char aibts[10];
+        _itoa_s(aiBatScore, aibts, 10);
+        _aiBatScore.setString(aibts);
+    }
+
+public:
+
+    Game() {
+        // Mensaje del inicio
+        title.setCharacterSize(90);
+        title.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
+        title.setFont(font);
+        title.setFillColor(Color::Color(21, 182, 185));
+        title.setString("     PONG");
+
+        startMessage.setCharacterSize(30);
+        startMessage.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
+        startMessage.setFont(font);
+        startMessage.setString("\n\n\n\n\n\n\n\n      Presiona las flechas para iniciar");
+
+        // Mensaje de victoria
+        finishMessage1.setCharacterSize(30);
+        finishMessage1.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
+        finishMessage1.setFont(font);
+        finishMessage1.setFillColor(Color::White);
+        finishMessage1.setString("                 ¡Ganaste!\n\n   Presiona cualquier tecla para reiniciar");
+
+        // Mensaje de derrota
+        finishMessage2.setCharacterSize(30);
+        finishMessage2.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
+        finishMessage2.setFont(font);
+        finishMessage2.setFillColor(Color::White);
+        finishMessage2.setString("                 Perdiste...\n\n   Presiona cualquier tecla para reiniciar");
+
+        //Mostrar puntaje del jugador
+        _BatScore.setCharacterSize(60);
+        _BatScore.setPosition(800.0f, 100.0f);
+        _BatScore.setFont(font);
+        _BatScore.setFillColor(Color::White);
+
+        //Mostrar puntaje de la IA
+        _aiBatScore.setCharacterSize(60);
+        _aiBatScore.setPosition(200.0f, 100.0f);
+        _aiBatScore.setFont(font);
+        _aiBatScore.setFillColor(Color::White);
+
+        updateBatScore();
+        updateAiBatScore();
+    }
+
+    void musicAndFont() {
+        //Cargando la fuente 
+        font.loadFromFile("VCR_OSD_MONO.ttf");
+
+        //Seteando la musica
+        buffer.loadFromFile("MainMusic.wav");
+        music.setBuffer(buffer);
+    }
+
+    //void logic() {
+
+    //}
+
+    void pong() {
         RenderWindow window(VideoMode(windowWidth, windowHeight), "Pong");
 
         //Creando los objetos
@@ -19,20 +99,20 @@ class Game {
         Ball ball(windowWidth / 2 + 50, windowHeight / 2);
         AIBat aibat(windowWidth - 40, windowHeight / 2);
 
+        musicAndFont();
+
         while (window.isOpen()) {
 
-            Font font;
-            font.loadFromFile("VCR_OSD_MONO.ttf");
-            
-            //Tengo que poder hacer que me cargue la musica mp3
-            // Music music;
-            // music.openFromFile("Audios/lady-of-the-80s-.mp3");
-            // music.setVolume(100);
-            //music.play();
+            // Limpiar y dibujar fondo
+            window.clear(Color(34, 34, 34, 100));
+
+            window.setFramerateLimit(480);
+
+            music.play();
 
             Event event;
             while (window.pollEvent(event)) {
-                if (event.type == Event::Closed) {
+                if (event.type == event.Closed) {
                     window.close();
                 }
             }
@@ -41,12 +121,11 @@ class Game {
             if (((event.type == event.KeyPressed) && event.key.code == Keyboard::Up)) {
                 bat.moveBatUp();
             }
-
-            if (((event.type == event.KeyPressed) && event.key.code == Keyboard::Down)) {
+            else if (((event.type == event.KeyPressed) && event.key.code == Keyboard::Down)) {
                 bat.moveBatDown();
             }
 
-            // Logica
+             // Logica
             ball.reboundSides();
             ball.passLeft();
             ball.passRight();
@@ -60,13 +139,13 @@ class Game {
             }
 
             if (ball.getBallFloatRect().top > (aibat.getAIBatFloatRect().top) + 50) {
-                if (aibatcounter % 60 == 0) {
+                if (aiBatCounter % 60 == 0) {
                     aibat.moveAIBatDown();
                 }
             }
 
             if (ball.getBallFloatRect().top < (aibat.getAIBatFloatRect().top) + 50) {
-                if (aibatcounter % 60 == 0) {
+                if (aiBatCounter % 60 == 0) {
                     aibat.moveAIBatUp();
                 }
             }
@@ -75,107 +154,62 @@ class Game {
                 aibat.AIBatSpeedReverse();
             }
 
-
-            //Texto y fuente
-            Text BatScore;
-
-            BatScore.setFont(font);
-            BatScore.setCharacterSize(60);
-            BatScore.setFillColor(sf::Color::White);
-
-            BatScore.setString(batscore);
-
-            /* TRANSFORMAR ESTO PARA BATSCORE Y AIBATSCORE
-            void updateLives() {
-                char pts[10];
-                _itoa_s(lives, pts, 10);
-                _lives.setString(pts);
-            }
-            */
-
-
-            // Mensaje de victoria
-            Text finishMessage1;
-
-            finishMessage1.setCharacterSize(50);
-            finishMessage1.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-            finishMessage1.setFont(font);
-            finishMessage1.setString("You win\n\nPress any key to play again.");
-
-            // Mensaje de derrota
-            Text finishMessage2;
-
-            finishMessage2.setCharacterSize(50);
-            finishMessage2.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-            finishMessage2.setFont(font);
-            finishMessage2.setString("You lose\n\nPress any key to play again.");
-
-            // Mensaje del inicio
-            Text startMessage;
-
-            startMessage.setCharacterSize(50);
-            startMessage.setPosition(windowWidth / 2 - 400, windowHeight / 2 - 100);
-            startMessage.setFont(font);
-            startMessage.setString("Welcome to Pong.\n\nPress any key to begin");
-
-            // Actualizacion
-            if (aibatcounter == 1000000) {
-                aibatcounter = 0;
+             // Actualizacion
+            if (aiBatCounter == 1000000) {
+                aiBatCounter = 0;
             }
 
-            aibatcounter++;
+            aiBatCounter++;
+            
             ball.update();
             bat.update();
             aibat.update();
 
-            // Dibujar
-            window.clear(Color(34, 34, 34, 100));
-
             // Textos e iniciacion //
-            
-            if (batscore == 10){
+
+            if (batScore == 10) {
                 //En caso de victoria
                 window.draw(finishMessage1);
                 ball.stop();
 
                 if (event.type == event.KeyPressed) {
-                    aiBatscore = 0;
-                    batscore = 0;
-                    ball.go();
+                    aiBatScore = 0;
+                    batScore = 0;
+                    ball.resetVelocity();
                 }
 
-            } else if (aiBatscore == 10) {
+            } else if (aiBatScore == 10) {
                 //En caso de derrota
                 window.draw(finishMessage2);
                 ball.stop();
 
                 if (event.type == event.KeyPressed) {
-                    aiBatscore = 0;
-                    batscore = 0;
-                    ball.go();
+                    aiBatScore = 0;
+                    batScore = 0;
+                    ball.resetVelocity();
                 }
 
-            }
-            else if (batscore == -1) {
+            } else if (batScore == -1) {
+                window.draw(title);
                 window.draw(startMessage);
                 ball.stop();
 
                 if (event.type == event.KeyPressed) {
-                    aiBatscore = 0;
-                    batscore = 0;
-                    ball.go();
+                    aiBatScore = 0;
+                    batScore = 0;
+
+                    //Mostrar puntaje de cada uno
+                    window.draw(_BatScore);
+                    window.draw(_aiBatScore);
+
+                    ball.resetVelocity();
                 }
 
-            }
-            else {
+            } else {
                 window.draw(bat.getBatObject());
                 window.draw(ball.getBallObject());
-
-                window.draw(BatScore);
                 window.draw((aibat.getAIBatObject()));
             }
-
-            //music.stop();
 
             window.display();
         }
