5c84cc955c00e32658b711830942b76ac55d4fbe
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-02-15 22:03:05 -0300

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "stdafx.h"
 
+// Altura de la pantalla
 int windowWidth = 1000;
+
+// Anchura de la pantalla
 int windowHeight = 700;
+
+// Configuraciones previas de los puntajes
 int batScore = -1;
 int aiBatScore = 0;
 
+// Configuracion previa de la IA
 int aiBatCounter = 0;
