452ca1ac0d5b0aa8f5055b8f23c6ee1b6ffcc640
Dante3830 <111542527+Dante3830@users.noreply.github.com>
2023-02-15 22:02:42 -0300

    Update and rename AIBat.h to AiBat.h

--- AiBat.h
+++ AiBat.h
@@ -2,59 +2,71 @@
 #include "stdafx.h"
 #include "Constants.h"
 
-class AIBat {
+class AiBat {
 private:
+    
     Vector2f aiBatPosition;
+    
+    // Velocidad de la IA
     double aiBatSpeed = 24;
+    
     RectangleShape aiBatObject;
 
 public:
 
-    AIBat(double x, double y) {
+    AiBat(double x, double y) {
         aiBatPosition.x = x;
         aiBatPosition.y = y;
-        aiBatObject.setSize(sf::Vector2f(10, 150));
+
+        // Seteando el tamano, color y posicion de la IA
+        aiBatObject.setSize(Vector2f(10, 150));
         aiBatObject.setPosition(aiBatPosition);
-        aiBatObject.setFillColor(sf::Color::Red);
+        aiBatObject.setFillColor(Color::Red);
     }
 
-    Vector2f getAIBatPosition() {
+    Vector2f getAiBatPosition() {
         return aiBatPosition;
     }
 
-    RectangleShape getAIBatObject() {
+    RectangleShape getAiBatObject() {
         return aiBatObject;
     }
 
-    FloatRect getAIBatFloatRect() {
+    FloatRect getAiBatFloatRect() {
         return aiBatObject.getGlobalBounds();
     }
 
-    void moveAIBatUp() {
+    // Mover la IA para arriba
+    void moveAiBatUp() {
         if (aiBatLimitsUp() == false) {
             aiBatPosition.y -= aiBatSpeed;
         }
     }
 
-    void moveAIBatDown() {
+    // Mover la IA para abajo
+    void moveAiBatDown() {
         if (aiBatLimitsDown() == false) {
             aiBatPosition.y += aiBatSpeed;
         }
     }
 
+    // Actualizar posicion de la IA
     void update() {
         aiBatObject.setPosition(aiBatPosition);
     }
 
+    // Limite para chocar con arriba
     bool aiBatLimitsUp() {
         return (aiBatPosition.y <= 0);
     }
 
+    // Limite para chocar con abajo
     bool aiBatLimitsDown() {
         return (aiBatPosition.y >= windowHeight - 150);
     }
 
-    void AIBatSpeedReverse() {
+    // Velocidad de la IA para arriba
+    void aiBatSpeedReverse() {
         aiBatSpeed *= -1;
     }
 
